3d9dd1ec2ce7c9a2c3791a9a6a9a75fa9c74b6f4
Abdul Raheem <shuja0094@gmail.com>
2023-08-28 16:48:30 +0500

    MIS-15 Password Reset

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
+const { Auth } = require("../handlers");
+const { authenticate } = require("../middlewares");
 const { auths } = require("./auth");
 const { invoice } = require("./invoice");
 const router = require("express").Router();
-
+const handler = new Auth();
 router.use("/auths", auths);
-router.use("/invoice", invoice);
+router.use("/invoice", handler.refresh, invoice);
 
 module.exports = { router };
