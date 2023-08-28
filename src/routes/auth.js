2c5db52aca89d983015894a945d2c39051eb6717
Abdul Raheem <shuja0094@gmail.com>
2023-08-28 13:53:18 +0500

    MIS-8 Change Password Done

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,9 +2,9 @@ const { Auth } = require("../handlers");
 const router = require("express").Router();
 const handler = new Auth();
 
-// router.get("/", handler.refresh);
 router.post("/", handler.registerUSer);
 router.post("/login", handler.login);
+router.patch("/change-password", handler.changePassword);
 
 module.exports = {
   auths: router,
